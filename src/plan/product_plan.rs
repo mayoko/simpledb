10bae17f2d96acfab33e521ca7181c2e2d5c6d61
mayoko <mayokoex@gmail.com>
2025-03-23 23:22:16 +0900

    add tests on ProductPlan

--- src/plan/product_plan.rs
+++ src/plan/product_plan.rs
@@ -55,3 +55,77 @@ impl ProductPlan {
         Ok(Self { p1, p2, schema })
     }
 }
+
+#[cfg(test)]
+mod product_plan_test {
+    use crate::{plan::plan::MockPlan, record::schema::FieldInfo};
+
+    use super::*;
+
+    fn setup_plan(
+        block_access_cost: u64,
+        record_access_cost: u64,
+        distinct_value_estimation: u64,
+        schema_field_name: String,
+    ) -> Box<dyn Plan> {
+        let schema = {
+            let mut schema = Schema::new();
+            schema.add_field(&schema_field_name, FieldInfo::Integer);
+            schema
+        };
+        let mut plan = MockPlan::new();
+        plan.expect_get_block_access_cost()
+            .returning(move || Ok(block_access_cost));
+        plan.expect_get_record_access_cost()
+            .returning(move || Ok(record_access_cost));
+        plan.expect_get_distinct_value_estimation()
+            .returning(move |field_name| {
+                if field_name == schema_field_name {
+                    Ok(distinct_value_estimation)
+                } else {
+                    Err(anyhow!("field not found"))
+                }
+            });
+        plan.expect_get_schema().return_const(schema);
+        Box::new(plan)
+    }
+
+    #[test]
+    fn block_access_cost_test() {
+        let p1 = setup_plan(10, 1000, 100, "field1".to_string());
+        let p2 = setup_plan(40, 2000, 50, "field2".to_string());
+        let product_plan = ProductPlan::new(p1, p2).unwrap();
+        assert_eq!(product_plan.get_block_access_cost().unwrap(), 40010); // 10 + 1000 * 40
+    }
+    #[test]
+    fn record_access_cost_test() {
+        let p1 = setup_plan(10, 1000, 100, "field1".to_string());
+        let p2 = setup_plan(40, 2000, 50, "field2".to_string());
+        let product_plan = ProductPlan::new(p1, p2).unwrap();
+        assert_eq!(product_plan.get_record_access_cost().unwrap(), 2_000_000); // 1000 * 2000
+    }
+    #[test]
+    fn record_distinct_value_estimation_test() {
+        let p1 = setup_plan(10, 1000, 100, "field1".to_string());
+        let p2 = setup_plan(40, 2000, 50, "field2".to_string());
+        let product_plan = ProductPlan::new(p1, p2).unwrap();
+        // p1 の distinct value
+        assert_eq!(
+            product_plan
+                .get_distinct_value_estimation("field1")
+                .unwrap(),
+            100
+        );
+        // p2 の distinct value
+        assert_eq!(
+            product_plan
+                .get_distinct_value_estimation("field2")
+                .unwrap(),
+            50
+        );
+        // 存在しない field
+        assert!(product_plan
+            .get_distinct_value_estimation("field3")
+            .is_err());
+    }
+}
