507ff592a433bd2bc65d4f20f5dbb66e83ef2d36
mayoko <mayokoex@gmail.com>
2025-03-24 23:42:04 +0900

    add tests on SelectPlan

--- src/plan/select_plan.rs
+++ src/plan/select_plan.rs
@@ -72,3 +72,194 @@ impl SelectPlan {
         Self { child, predicate }
     }
 }
+
+#[cfg(test)]
+mod select_plan_test {
+    use crate::{
+        plan::{
+            expression::Expression,
+            plan::MockPlan,
+            predicate::ProductPredicate,
+            term::{EqualTerm, Term},
+        },
+        query::constant::Constant,
+        record::schema::FieldInfo,
+    };
+
+    use super::*;
+    use anyhow::anyhow;
+
+    fn setup_plan(block_access_cost: u64, record_access_cost: u64) -> Box<dyn Plan> {
+        let schema = {
+            let mut schema = Schema::new();
+            schema.add_field("field1", FieldInfo::Integer);
+            schema.add_field("field2", FieldInfo::String(10));
+            schema.add_field("field3", FieldInfo::String(10));
+            schema
+        };
+        let mut plan = MockPlan::new();
+        plan.expect_get_block_access_cost()
+            .returning(move || Ok(block_access_cost));
+        plan.expect_get_record_access_cost()
+            .returning(move || Ok(record_access_cost));
+        plan.expect_get_distinct_value_estimation()
+            .returning(move |field_name| {
+                if field_name == "field1" {
+                    Ok(10)
+                } else if field_name == "field2" {
+                    Ok(20)
+                } else if field_name == "field3" {
+                    Ok(50)
+                } else {
+                    Err(anyhow!("field not found"))
+                }
+            });
+        plan.expect_get_schema().return_const(schema);
+        Box::new(plan)
+    }
+
+    fn setup_predicate() -> Predicate {
+        let predicate: ProductPredicate = ProductPredicate::new(vec![
+            // field1 = 1
+            Term::Equal(EqualTerm::new(
+                Expression::Field("field1".to_string()),
+                Expression::Constant(Constant::Int(1)),
+            )),
+            // and field2 = field3
+            Term::Equal(EqualTerm::new(
+                Expression::Field("field2".to_string()),
+                Expression::Field("field3".to_string()),
+            )),
+        ]);
+        Predicate::Product(predicate)
+    }
+
+    #[test]
+    fn block_access_cost_test() {
+        let p = setup_plan(10, 1000);
+        let select_plan = SelectPlan::new(p, Box::new(setup_predicate()));
+        assert_eq!(select_plan.get_block_access_cost().unwrap(), 10); // same as child's block access cost
+    }
+
+    #[test]
+    fn record_access_cost_test_for_no_predicate() {
+        let p = setup_plan(10, 1000);
+        let predicate = Predicate::Product(ProductPredicate::new(vec![]));
+        let select_plan = SelectPlan::new(p, Box::new(predicate));
+        // 制限がなければ、元の Plan の record access cost がそのまま使われる
+        assert_eq!(select_plan.get_record_access_cost().unwrap(), 1000);
+    }
+
+    #[test]
+    fn record_access_cost_test_for_single_equal_with_constant_condition() {
+        let p = setup_plan(10, 1000);
+        let predicate = Predicate::Product(ProductPredicate::new(vec![
+            // field1 = 1
+            Term::Equal(EqualTerm::new(
+                Expression::Field("field1".to_string()),
+                Expression::Constant(Constant::Int(1)),
+            )),
+        ]));
+        let select_plan = SelectPlan::new(p, Box::new(predicate));
+        assert_eq!(select_plan.get_record_access_cost().unwrap(), 100); // 1000 / 10
+    }
+
+    #[test]
+    fn record_access_cost_test_for_single_equal_with_field_condition() {
+        let p = setup_plan(10, 1000);
+        let predicate = Predicate::Product(ProductPredicate::new(vec![
+            // field2 = field3
+            Term::Equal(EqualTerm::new(
+                Expression::Field("field2".to_string()),
+                Expression::Field("field3".to_string()),
+            )),
+        ]));
+        let select_plan = SelectPlan::new(p, Box::new(predicate));
+        assert_eq!(select_plan.get_record_access_cost().unwrap(), 20); // 1000 / max(20, 50)
+    }
+
+    #[test]
+    fn record_access_cost_test_for_multiple_condition() {
+        let p = setup_plan(10, 1000);
+        let predicate = Predicate::Product(ProductPredicate::new(vec![
+            // field1 = 1 and field2 = field3
+            Term::Equal(EqualTerm::new(
+                Expression::Field("field1".to_string()),
+                Expression::Constant(Constant::Int(1)),
+            )),
+            Term::Equal(EqualTerm::new(
+                Expression::Field("field2".to_string()),
+                Expression::Field("field3".to_string()),
+            )),
+        ]));
+        let select_plan = SelectPlan::new(p, Box::new(predicate));
+        assert_eq!(select_plan.get_record_access_cost().unwrap(), 2); // 1000 / (max(20, 50) * 10)
+    }
+
+    #[test]
+    fn distinct_value_estimation_test_for_no_predicate() {
+        let p = setup_plan(10, 1000);
+        let predicate = Predicate::Product(ProductPredicate::new(vec![]));
+        let select_plan = SelectPlan::new(p, Box::new(predicate));
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field1").unwrap(),
+            10
+        );
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field2").unwrap(),
+            20
+        );
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field3").unwrap(),
+            50
+        );
+    }
+    #[test]
+    fn distinct_value_estimation_test_for_constant_equal_condition() {
+        let p = setup_plan(10, 1000);
+        let predicate = Predicate::Product(ProductPredicate::new(vec![
+            // field1 = 1
+            Term::Equal(EqualTerm::new(
+                Expression::Field("field1".to_string()),
+                Expression::Constant(Constant::Int(1)),
+            )),
+        ]));
+        let select_plan = SelectPlan::new(p, Box::new(predicate));
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field1").unwrap(),
+            1
+        );
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field2").unwrap(),
+            20
+        );
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field3").unwrap(),
+            50
+        );
+    }
+    #[test]
+    fn distinct_value_estimation_test_for_field_equal_condition() {
+        let p = setup_plan(10, 1000);
+        let predicate = Predicate::Product(ProductPredicate::new(vec![
+            // field2 = field3
+            Term::Equal(EqualTerm::new(
+                Expression::Field("field2".to_string()),
+                Expression::Field("field3".to_string()),
+            )),
+        ]));
+        let select_plan = SelectPlan::new(p, Box::new(predicate));
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field1").unwrap(),
+            10
+        );
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field2").unwrap(),
+            20
+        );
+        assert_eq!(
+            select_plan.get_distinct_value_estimation("field3").unwrap(),
+            20
+        );
+    }
+}
